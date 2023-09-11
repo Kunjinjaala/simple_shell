f49267aeb4dc13cc11cbe1e3b4564e9cd5a49723
kunjinjaala <mhilmi9236@yahoo.com>
2023-09-11 07:28:31 -0700

    simple shell

---
+++ betty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	printf("Hi, Checker!\n");
+
+	for (i > 0; i < 10; i++)
+	{
+		printf("Single digit Integers  %d\n", i);
+	}
+
+	return (0);
+}
